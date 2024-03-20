c59d72a38923816f6e5cc35ba0fced374e0c0ef3
GOUTHAM183 <gouthamlucky68@gmail.com>
2024-03-20 09:39:51 -0500

    Second Commit

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
     public static void main(String[] args){
         System.out.println("--Java-Jenkins project--");
         System.out.println(new Date());
+        System.out.println("Hello, Jenkins");
     }
 }
